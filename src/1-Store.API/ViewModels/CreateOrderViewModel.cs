f0e367afcfe4ea916121409b8db78d18de25e2d5
Gabriel Bobrov <bielbobrov@hotmail.com>
2022-04-05 11:25:42 -0300

    Add validation to price

--- src/1-Store.API/ViewModels/CreateOrderViewModel.cs
+++ src/1-Store.API/ViewModels/CreateOrderViewModel.cs
@@ -6,6 +6,7 @@ namespace Store.API.ViewModels
     public class CreateOrderViewModel
     {
         [Required(ErrorMessage = "O preço não pode ser vazio.")]
+        [Range(0.1, Int32.MaxValue, ErrorMessage = "O preço deve ser maior que 0.")]
         public decimal Price { get; set; }
 
         [Required(ErrorMessage = "O id do cliente não pode ser vazio.")]
